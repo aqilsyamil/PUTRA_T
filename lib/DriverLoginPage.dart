4629f3ae57d4c722e9235743461d99d97dd4c539
Aqil <aqilsyamil@icloud.com>
2023-12-25 19:37:27 +0800

    New Version

--- lib/DriverLoginPage.dart
+++ lib/DriverLoginPage.dart
@@ -1,6 +1,41 @@
 import 'package:flutter/material.dart';
 
-class DriverLoginPage extends StatelessWidget {
+class DriverLoginPage extends StatefulWidget {
+  @override
+  _DriverLoginPageState createState() => _DriverLoginPageState();
+}
+
+class _DriverLoginPageState extends State<DriverLoginPage> {
+  final TextEditingController driverIdController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  bool isButtonEnabled = false;
+
+  @override
+  void initState() {
+    super.initState();
+    // Add listeners to text controllers to update button state
+    driverIdController.addListener(updateButtonState);
+    passwordController.addListener(updateButtonState);
+  }
+
+  void updateButtonState() {
+    final driverIdFilled = driverIdController.text.isNotEmpty;
+    final passwordValid = passwordController.text.length >= 8;
+
+    setState(() {
+      isButtonEnabled = driverIdFilled && passwordValid;
+    });
+  }
+
+  @override
+  void dispose() {
+    // Dispose of text controllers to avoid memory leaks
+    driverIdController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,7 +60,7 @@ class DriverLoginPage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.start, // Align at the top
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            SizedBox(height: 60), // Add a SizedBox with 60 pixels of height
+            SizedBox(height: 40), // Add a SizedBox with 60 pixels of height
             Align(
               alignment: Alignment.centerLeft,
               child: Column(
@@ -54,9 +89,9 @@ class DriverLoginPage extends StatelessWidget {
               ),
             ),
             SizedBox(height: 40),
-            _buildTextField("Driver ID"),
+            _buildTextField("Driver ID", controller: driverIdController),
             SizedBox(height: 15),
-            _buildTextField("Password", isPassword: true),
+            _buildTextField("Password", isPassword: true, controller: passwordController),
             SizedBox(height: 15), // Add spacing below the password field
             Center(
               child: Row(
@@ -93,18 +128,20 @@ class DriverLoginPage extends StatelessWidget {
                 ],
               ),
             ),
-            SizedBox(height: 50), // Adjust the spacing above the "Continue" button
+            SizedBox(height: 30), // Adjust the spacing above the "Continue" button
             Container(
               width: 334,
               height: 65,
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(10),
-                color: Color(0xFF00D161), // Set background color to match the AppBar color
+                color: isButtonEnabled ? Color(0xFF00D161) : Color(0xFF00D161).withOpacity(0.5), // Set background color to match the AppBar color
               ),
               child: TextButton(
-                onPressed: () {
+                onPressed: isButtonEnabled
+                    ? () {
                   // Handle login button press
-                },
+                }
+                    : null, // Disable the button if conditions are not met
                 child: Text(
                   'Continue',
                   style: TextStyle(
@@ -123,7 +160,7 @@ class DriverLoginPage extends StatelessWidget {
     );
   }
 
-  Widget _buildTextField(String label, {bool isPassword = false}) {
+  Widget _buildTextField(String label, {bool isPassword = false, TextEditingController? controller}) {
     return Container(
       width: 334,
       height: 52,
@@ -138,6 +175,7 @@ class DriverLoginPage extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 10.0),
         child: TextField(
+          controller: controller, // Assign the controller
           obscureText: isPassword,
           decoration: InputDecoration(
             border: InputBorder.none,
